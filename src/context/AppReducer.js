c6ad9d55c35fcf0398cc4767995b05f3cf70d05a
ItuThaGoat <itumelengmochabe@gmail.com>
2020-05-15 15:00:08 +0200

    Added a global reducer
    Added a default global reducer with a default case.

---
+++ src/context/AppReducer.js
@@ -0,0 +1,7 @@
+// Reducer specify what happens to the state depending on certain actions
+export default (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,5 +1,6 @@
 // We will create our context. We only need one context due to the size of the appplication
 import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
 // Initial state
 const initialState = {
@@ -13,3 +14,16 @@ const initialState = {
 
 // Create the context
 export const GlobalContext = createContext(initialState);
+
+// We need to create a provider which will provide global context and wrap all the
+// children components inside.
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
